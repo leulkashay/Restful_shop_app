e7aba3e00fe8f3c0f16e29c2c25c3ef2b4f8a6b0
leulkashay <leulkashay2012@gmail.com>
2023-07-19 07:37:24 +0300

    first task

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const port = process.env.PORT || 7000;
+const app = require('./app');
 
-const server = http.createServer();
+const server = http.createServer(app);
 
 server.listen(port);
